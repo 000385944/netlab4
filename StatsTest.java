d9e4882e153ca3f2f293649b7b7fedf6c643f572
000385944 <000385944@mohawkcollege.ca>
2017-12-21 19:04:06 +0530

    Added more tests, code coverage now 96%

--- StatsTest.java
+++ StatsTest.java
@@ -11,11 +11,38 @@ import org.junit.Test;
 public class StatsTest {
 	
 	private static final ArrayList<Integer> numIntList = new ArrayList<>(Arrays.asList(9,2,7,4,5,6,3,8,1,10));
+	private static final ArrayList<Integer> numNegIntList = new ArrayList<>(Arrays.asList(-9,-2,-7));
 	private static final ArrayList<Double> numDoubleList = new ArrayList<>(Arrays.asList(2.2, 100.0, 17.5, 30.2, 31.1));
 
 	@Test
+	public void testClassInitialiser() {
+		jblink_stats JBLinkStats = new jblink_stats();
+		assertTrue(JBLinkStats.getClass() == jblink_stats.class);
+	}
+	
+	@Test
 	public void testAvgUsingIntegerList() {
 		assertEquals(5.50, jblink_stats.avg(numIntList, true), 0.0);
+	}
+	
+	@Test
+	public void testAvgUsingIntegerListWithIncPositive() {
+		assertEquals(5.50, jblink_stats.avg(numIntList, false), 0.0);
+	}
+	
+	@Test
+	public void testAvgUsingNegativeIntegerList() {
+		assertEquals(-6, jblink_stats.avg(numNegIntList, true), 0.0);
+	}
+	
+	@Test
+	public void testAvgUsingNegativeIntegerIncPositiveList() {
+		try {
+			jblink_stats.avg(numNegIntList, false);
+		} catch (Exception e) {
+			assertEquals(e.getLocalizedMessage(), "no values are > 0");
+			assertEquals(e.getClass(), java.lang.IllegalArgumentException.class);
+		}
 	}
 	
 	@Test
@@ -26,6 +53,11 @@ public class StatsTest {
 	@Test
 	public void testSumUsingIntegerList() {
 		assertEquals(55, jblink_stats.sum(numIntList, true), 0.0);
+	}
+	
+	@Test
+	public void testSumUsingIntegerListWithIncPositive() {
+		assertEquals(55, jblink_stats.sum(numIntList, false), 0.0);
 	}
 	
 	@Test
@@ -41,6 +73,17 @@ public class StatsTest {
 	@Test
 	public void testMedUsingDoubleList() {
 		assertEquals(30.2, jblink_stats.med(numDoubleList), 0.0);
+	}
+	
+	@Test
+	public void testSdUsingSmallIntegerList() {
+		try {
+			jblink_stats.sd(new ArrayList<>(Arrays.asList(9)));
+		} catch (Exception e) {
+			assertEquals(e.getLocalizedMessage(), "Size of array must be greater than 1");
+			assertEquals(e.getClass(), java.lang.IllegalArgumentException.class);
+		}
+			
 	}
 
 	@Test
